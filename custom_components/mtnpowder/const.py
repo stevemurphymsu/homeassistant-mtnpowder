22164a62ac5a5eb462be49a30fc355de825b6741
stevemurphymsu <stevemurphymsu@gmail.com>
2025-12-20 12:56:40 -0500

    Changed domain back to mtnpowder for debugging purposes.

--- custom_components/mtnpowder/const.py
+++ custom_components/mtnpowder/const.py
@@ -1,6 +1,6 @@
 """Constants for the mtnpowder integration."""
 
-DOMAIN = "alterra_mountian_company"
+DOMAIN = "mtnpowder"
 DEFAULT_NAME = "MtnPowder"
 DEFAULT_SCAN_INTERVAL = 300
 FEED_URL = "https://mtnpowder.com/feed/"
